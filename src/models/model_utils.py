Add ConvMixer model Solve batch size mistery + set up step_per_epoch normalisation for each training config Fix/add Combi sheduler for warmup + Pleateau Add some verbosity Change testing encoding for a more effficient solution: Subsampling at the source! Change the plotting type in wandb, let see if this is actually better..

src/models/model_utils.py
@@ -36,25 +36,11 @@ class PositionalEncoding(nn.Module):
 		return self.dropout(x)
 
 
-#  LIL Encoding NETWORK
-class BaseModel(nn.Module):
-	def __init__(self):
-		super(EncoderNet, self).__init__()
-
-	def encoder(self, x):         
-		return x
-	
-	def expender(self, x) :
-		return x
-	
-	def forward(self, x):
-		return x
-	
-
 def load_model():
 	model = eval("models." + params.model_name)().to(params.device)
 	if params.verbose:
 		print(model)
+		print("Total number of model's parameters", sum(p.numel() for p in model.parameters()))
 	return model
 
 

src/models/transformer3.py
@@ -130,10 +130,10 @@ class Transformer3(nn.Module):
 	def encode(self, x):
 		if params.data_use_position:
 			x = self.positional_encoder(x)
-		return self.encoder(x)		
+		return self.encoder(x)
 
 	def classify(self, x):
-		return self.expender(x)		
+		return self.expender(x)
 	
 	def forward(self, x):
 		x = self.encode(x)

src/params.py
@@ -41,16 +41,17 @@ same_positions = True   # If the concatenation should be done diagonal to positi
 ############
 #   Train
 ############
-batch_size = 64
+batch_size = 1024
+steps_per_epoch = 100   # Provide data independant granularity of the training process
 nb_epochs = 10000
-test_interval = 250
+test_interval = 10
 
 
 ############
 #   Optim
 ############
 optimizer = "Adam"
-sheduler = "combi"    #"warmup" plateau "combi" for the combination of both
+sheduler = "plateau"    #"warmup" plateau "combi" for the combination of both
 warmup_steps = 50
 learning_rate = 1e-3
 lr_limit = 1e-4
@@ -60,18 +61,18 @@ patience = 250
 ###########
 #   Loss
 ###########
-loss = "VicregLoss"  #"vicreg" #"adversarial" #"triplet3" #"triplet" #"vicreg"
+loss = "VicregLoss"  #"VicregLoss" #"AdversarialLoss" #"CrossentropyLoss" #"triplet"
 lambda_triplet = 10
 triplet_mmargin = 1
 lambda_distance = 11    #14
-lambda_std = 4         #1.2
+lambda_std = 1         #1.2
 lambda_cov = 4          #4
 
 
 ############
 #   Model
 ############
-model_name = "Transformer3" #"advCNN1" #"Transformer3"
+model_name = "ConvMixer" #"Transformer3" #"advCNN1" #"Transformer3"
 latent_dimention = 256
 expender_out = 256
 use_extender = False
@@ -111,7 +112,7 @@ window_size = 16
 ##############
 #   System   #
 ##############
-save_model = True   # On test section
+save_model = False   # On test section
 use_gpu = True
 device = "cuda" if torch.cuda.is_available() and use_gpu else "cpu"
 verbose = True

src/test.py
@@ -21,16 +21,22 @@ def normdata(x):
     return x
 
 
-def encode_data(mymodel, dataloader):
+def encode_data(mymodel, dataloader, encode_even_less=False):
     mymodel.eval()
     def loop_on_loader(loader):
         loader.dataset.testset = True
         encs, labs = [], []
+        count = 0
+        amt2encode = len(loader.dataset) * params.data_test_rate
+        if encode_even_less: amt2encode * params.data_test_rate
         for i, (batchX, batchY) in enumerate(loader):
             # Compute encoded version of the data by our embedding model
             encs = encs + mymodel.encode(batchX).tolist()
             # Gather device labels accordingly (eventually randomly enumerated)
             labs = labs + batchY[:, 0].tolist()
+            count += batchX.shape[0]
+            if count > amt2encode:
+                break
         return encs, labs
     
     # Split differently according to the dataloader organisation
@@ -221,7 +227,7 @@ def testing_model(training_loaders, validation_loader, model, logger):
     model.eval()
     logger.save_model(model)
 
-    encoded_train, labels_train = encode_data(model, training_loaders)
+    encoded_train, labels_train = encode_data(model, training_loaders, encode_even_less=True)
     encoded_test, labels_test = encode_data(model, validation_loader)
 
     enc_time = time.time()
@@ -234,14 +240,14 @@ def testing_model(training_loaders, validation_loader, model, logger):
 
     # Selecting a subset of size params.data_test_rate for reid evalution
     mask = np.full(len(encoded_test), False)
-    mask[:int(len(encoded_test)*params.data_test_rate)] = True
+    mask[:int(len(encoded_test))] = True #*params.data_test_rate)] = True
     np.random.shuffle(mask)
     reid_evaluation(np.array(encoded_test)[mask], np.array(labels_test)[mask], logger)
     reid_time = time.time()
     print("[Test]: time for reid on test data", reid_time - scatter_time)
 
     mask2 = np.full(len(labels_train), False)
-    mask2[:int(len(labels_train)*params.data_test_rate*params.data_test_rate)] = True
+    mask2[:int(len(labels_train))] = True #*params.data_test_rate*params.data_test_rate)] = True
     np.random.shuffle(mask2)
     reid_evaluation_test2train_NN(np.array(encoded_test)[mask], np.array(encoded_train)[mask2], np.array(labels_test)[mask], np.array(labels_train)[mask2], logger)
     reid2_time = time.time()